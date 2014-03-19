ea571084049824b9b1fb5ced1593768c2575b2a4
Patrick van der Tuin <patricktuin@planet.nl>
2014-03-20 00:24:48 +0100

    Adjusted

--- file.rb
+++ file.rb
@@ -1,29 +1,80 @@
+#################################
+# Search through (log)files     #
+# Version 0.12                  #
+# Last change date: 19-3-2014   #
+#################################
+#!/usr/bin/env ruby
+
 #Get the input parameter to search for
-puts "Search for?"
-input = gets.chomp
+#puts "Search for?"
+#input = gets.chomp
+input = "iioi58pt"
 
 #Check if matches should be displayed or only count should be done
-puts "Display for search results? Y/N/U (Yes / No / Unique values only)"
-display = gets.chomp
+#puts "Display for search results? Y/N/U (Yes / No / Unique values only)"
+#display = gets.chomp
+display = "n"
 display = display.downcase
 
 #create an array where search results will be added
 array = Array.new
 
 #Open the file
-file = File.new("sso10.txt", "r")
+file = File.new("sso12.txt", "r")
 
+
+linecount=0
+while (line = file.gets)
+	linecount += 1
+	if username = /(username=[^&]*)/.match(line)
+		array.push username
+	else
+	end
+end
+
+string = ""
+array.each do |usernames|
+	string = string + usernames.to_s + " "
+end
+
+strings = string.split(" ")
+
+test =Array.new
+
+frequencies = Hash.new(0)
+strings.each { |word| frequencies[word] += 1 }
+frequencies = frequencies.sort_by {|a, b| b }
+#frequencies.reverse!
+frequencies.each { |word, frequency| test.push [word] + [frequency.to_s]}
+
+
+puts test.map {|row| row[1]}
+
+#puts array
+#puts linecount
+#puts array.count
+
+=begin
 linecount = 0
 #Go through the whole file
 while (line = file.gets)
 linecount += 1
+
 #Devide current line into substrings
-	substrings = line.split("&")
+substrings = line.split("&")
+    
+#Regular expression for every line
+username = /(username=[^&]*)/.match(line)
+time = /(timestamp=[0-9]*)/.match(line)
+#u=username.split("=")
 
 #For every line check for "input" present. If present return substring where "input" is present
 	substrings.each do |sub|
 	  if sub.include? input
-	    array.push sub
+         #Split the substring. This will retrieve username only
+         #subusername = username.split("=")      
+	    array.push [[username],[time]]
+	    #puts line          
 	  else
 	 end
 	end
@@ -38,13 +89,22 @@ result = case display
 	else
 end
 
+test = Array.new
+
+array.each do |x|
+	test.push x
+end
+
+puts test
+
 #Show the totals
 puts ""
 puts "Total lines in input file: #{linecount}"
-puts "Total search count: #{array.count}"
-puts "Unique search count: #{array.uniq.count}"
+puts "Total search count: #{test.count}"
+puts "Unique search count: #{test.uniq.count}"
 puts "Duplicate search count: #{array.count - array.uniq.count}"
 puts ""
+=end
 
 #Close the file
 file.close
